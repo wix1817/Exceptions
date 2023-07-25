a4c9df80b2c35295abdef82ddf8b1eb244199863
Aliaksei Pustavy <wix1817@gmail.com>
2023-07-25 18:29:09 +0200

    Making full refactoring

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
 ﻿using Exceptions;
-Console.WriteLine(UserDataChecker.CheckData("KekMachine", "1234", "1234"));
+
+do
+{
+    Console.WriteLine("Input login: ");
+    var _login = Console.ReadLine();
+    Console.WriteLine("Input password: ");
+    var _password = Console.ReadLine();
+    Console.WriteLine("Confirm password: ");
+    var _confirmPassword = Console.ReadLine();
+    try
+    {
+        if (UserDataProvider.CheckData(_login, _password, _confirmPassword))
+        {
+            Console.WriteLine("True");
+            break;
+        }
+    }
+    catch (WrongLoginException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    catch (WrongPasswordException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+} while (true);
+
---
+++ UserDataProvider.cs
@@ -0,0 +1,31 @@
+﻿namespace Exceptions;
+
+public static class UserDataProvider
+{
+    private static void LoginVerification(string login)
+    {
+        if (login.Length is < 1 or > 20)
+            throw new WrongLoginException("Input correct login");
+        if (login.Contains(' '))
+            throw new WrongLoginException("Input login without spaces");
+    }
+
+    private static void PasswordVerification(string password, string confirmPassword)
+    {
+        if (password.Length is < 1 or > 20)
+            throw new WrongPasswordException("Wrong length of password");
+        if (!password.Any(Char.IsDigit))
+            throw new WrongPasswordException("Input password with digits");
+        if (password.Contains(' '))
+            throw new WrongPasswordException("Input password without spaces");
+        if (confirmPassword != password)
+            throw new WrongPasswordException("Password not equals Confirm password");
+    }
+    
+    public static bool CheckData(string login, string password, string confirmPassword)
+    {
+        LoginVerification(login);
+        PasswordVerification(password, confirmPassword);
+        return true;
+    }
+}
